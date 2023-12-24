752acfd12aa4e1e65ebba2f6ac11704d11d7b44e
Olga B <o.lgaromanovna@yandex.ru>
2023-12-24 14:28:55 +0300

    changed files

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -3,22 +3,22 @@ from my_book_project.pages.authorization_page import AuthorizationPage
 from my_book_project.pages.main_page import MainPage
 
 
-def test_authorization_form():
-    main_page = MainPage()
-    main_page.open()
-    main_page.button_enter()
-    authorization_page = AuthorizationPage()
-    authorization_page.fill_email(olga.email)
-    authorization_page.fill_password(olga.password)
-    authorization_page.button_authorize()
-    authorization_page.should_be_visible_email()
-
-def test_authorization_form_with_wrong_password():
-    main_page = MainPage()
-    main_page.open()
-    main_page.button_enter()
-    authorization_page = AuthorizationPage()
-    authorization_page.fill_email(olga.email)
-    authorization_page.fill_password(olga.password[-1])
-    authorization_page.button_authorize()
-    authorization_page.should_be_error()
+# def test_authorization_form():
+#     main_page = MainPage()
+#     main_page.open()
+#     main_page.button_enter()
+#     authorization_page = AuthorizationPage()
+#     authorization_page.fill_email(olga.email)
+#     authorization_page.fill_password(olga.password)
+#     authorization_page.button_authorize()
+#     authorization_page.should_be_visible_email()
+#
+# def test_authorization_form_with_wrong_password():
+#     main_page = MainPage()
+#     main_page.open()
+#     main_page.button_enter()
+#     authorization_page = AuthorizationPage()
+#     authorization_page.fill_email(olga.email)
+#     authorization_page.fill_password(olga.password[-1])
+#     authorization_page.button_authorize()
+#     authorization_page.should_be_error()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -3,25 +3,25 @@ from my_book_project.pages.main_page import MainPage
 from my_book_project.pages.registration_page import RegistrationPage
 
 
-def test_registration_form():
-    main_page = MainPage()
-    main_page.open()
-    main_page.button_enter()
-    registration_page = RegistrationPage()
-    registration_page.button_registration()
-    registration_page.fill_email(resources.email)
-    registration_page.fill_password(resources.password)
-    registration_page.button_register()
-    registration_page.should_have_text()
-
-def test_registration_form_with_invalid_email():
-    main_page = MainPage()
-    main_page.open()
-    main_page.button_enter()
-    registration_page = RegistrationPage()
-    registration_page.button_registration()
-    registration_page.fill_email(resources.email[3])
-    registration_page.fill_password(resources.password)
-    registration_page.button_register()
-    registration_page.should_be_error()
+# def test_registration_form():
+#     main_page = MainPage()
+#     main_page.open()
+#     main_page.button_enter()
+#     registration_page = RegistrationPage()
+#     registration_page.button_registration()
+#     registration_page.fill_email(resources.email)
+#     registration_page.fill_password(resources.password)
+#     registration_page.button_register()
+#     registration_page.should_have_text()
+#
+# def test_registration_form_with_invalid_email():
+#     main_page = MainPage()
+#     main_page.open()
+#     main_page.button_enter()
+#     registration_page = RegistrationPage()
+#     registration_page.button_registration()
+#     registration_page.fill_email(resources.email[3])
+#     registration_page.fill_password(resources.password)
+#     registration_page.button_register()
+#     registration_page.should_be_error()
 
