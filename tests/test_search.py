022ee5bd4bd1bb9a0dd74b995ac08f4d1c9fb541
Olga B <o.lgaromanovna@yandex.ru>
2023-12-27 20:32:11 +0300

    changed files

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,7 +1,13 @@
+import allure
 from my_book_project.pages.main_page import MainPage
 from my_book_project.pages.search_page import SearchPage
 
 
+@allure.id("28436")
+@allure.title("Проверка поисковой строки")
+@allure.tag("search")
+@allure.label("suite", "Поисковая строка")
+@allure.label("owner", "allure8")
 def test_search_string():
     main_page = MainPage()
     main_page.open()
